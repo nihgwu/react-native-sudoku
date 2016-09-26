1dc2f1d00278c667dc723c5e6c15d1d26fad30c1
Neo <nihgwu@live.com>
2016-09-26 11:22:48 +0800

    update

--- app/components/Puzzle.js
+++ app/components/Puzzle.js
@@ -21,22 +21,24 @@ function formatTime(elapsed) {
 const CellSize = 15;
 
 class Puzzle extends Component {
-	onPress = () => {
-		this.props.onPress && this.props.onPress(this.props.puzzle);
-	}
+  onPress = () => {
+    this.props.onPress && this.props.onPress(this.props.puzzle);
+  }
   render() {
     const { puzzle, elapsed } = this.props;
     return (
       <View style={styles.container} >
-	      {!!puzzle && puzzle.map((item, idx) => (
-	      	<View key={idx} style={[styles.cell, item != null && {backgroundColor: 'khaki'}]} />
-	    	))}
-	    	<View style={styles.timer}>
-	    		<Text style={styles.timerText}>{formatTime(elapsed)}</Text>
-	    	</View>
-    		<Touchable style={styles.button} onPress={this.onPress}>
-    			<Image style={styles.image} source={require('../images/play.png')} />
-    		</Touchable>
+        <View style={styles.board} >
+          {!!puzzle && puzzle.map((item, idx) => (
+            <View key={idx} style={[styles.cell, item != null && {backgroundColor: 'khaki'}]} />
+          ))}
+        </View>
+        <View style={styles.timer}>
+          <Text style={styles.timerText}>{formatTime(elapsed)}</Text>
+        </View>
+        <Touchable style={styles.button} onPress={this.onPress}>
+          <Image style={styles.image} source={require('../images/play.png')} />
+        </Touchable>
       </View>
     );
   }
@@ -46,10 +48,14 @@ const styles = StyleSheet.create({
   container: {
     width: CellSize * 9,
     height: CellSize * 9,
+    marginHorizontal: CellSize / 2,
+  },
+  board: {
+    width: CellSize * 9,
+    height: CellSize * 9,
     flexDirection: 'row',
     flexWrap: 'wrap',
     backgroundColor: 'lightyellow',
-    marginHorizontal: CellSize / 2,
   },
   cell: {
     width: CellSize,
@@ -57,37 +63,37 @@ const styles = StyleSheet.create({
     backgroundColor: 'lightyellow',
   },
   timer: {
-  	position: 'absolute',
-  	left: 0,
-  	right: 0,
-  	bottom: CellSize,
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: CellSize,
   },
   timerText: {
-  	color: 'peru',
-  	opacity: 0.8,
-  	textAlign: 'center',
-  	fontSize: 14,
+    color: 'peru',
+    opacity: 0.8,
+    textAlign: 'center',
+    fontSize: 14,
     fontWeight: '100',
     fontFamily: 'Menlo',
     backgroundColor: 'transparent',
   },
   button: {
-  	position: 'absolute',
-  	bottom: CellSize * 9 / 2 - 24,
-  	left: CellSize * 9 / 2 - 24,
-  	width: 48,
-  	height: 48,
-  	borderRadius: 24,
-  	backgroundColor: '#fff8',
-  	alignItems: 'center',
-  	justifyContent: 'center',
+    position: 'absolute',
+    top: CellSize * 9 / 2 - 24,
+    left: CellSize * 9 / 2 - 24,
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    backgroundColor: '#fff8',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   image: {
-  	left: 2,
-  	width: 30,
-  	height: 30,
-  	tintColor: 'peru',
-  	opacity: 0.5,
+    left: 2,
+    width: 30,
+    height: 30,
+    tintColor: 'peru',
+    opacity: 0.5,
   },
 });
 
